d7e59e7da4c05ab5fc6d11a33087d170c0e48994
Marco Happe <marco.happe@icloud.com>
2020-03-04 19:24:23 +0100

    added PetTypes to Bootstrap (startup). closes #20

--- src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
+++ src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package happe.marco.petclinic.bootstrap;
 
 import happe.marco.petclinic.model.Owner;
+import happe.marco.petclinic.model.PetType;
 import happe.marco.petclinic.model.Vet;
 import happe.marco.petclinic.services.OwnerService;
+import happe.marco.petclinic.services.PetTypeService;
 import happe.marco.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType marcosDog = new PetType();
+        marcosDog.setName("Senta");
+        PetType savedMarcosDog = petTypeService.save(marcosDog);
+
+        PetType lisasCat = new PetType();
+        lisasCat.setName("Snowball");
+        PetType savedLisasCat = petTypeService.save(lisasCat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Marco");
