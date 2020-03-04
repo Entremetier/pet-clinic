c6b6b491fb32f4f2d76bf8a95ca36b27d97453e4
Marco Happe <marco.happe@icloud.com>
2020-03-04 18:59:46 +0100

    added specialities, added to vet. closes #16

---
+++ src/main/java/happe/marco/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package happe.marco.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String describtion;
+
+    public String getDescribtion() {
+        return describtion;
+    }
+
+    public void setDescribtion(String describtion) {
+        this.describtion = describtion;
+    }
+}
--- src/main/java/happe/marco/petclinic/model/Vet.java
+++ src/main/java/happe/marco/petclinic/model/Vet.java
@@ -1,4 +1,17 @@
 package happe.marco.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> getSpecialties = new HashSet<>();
+
+    public Set<Speciality> getGetSpecialties() {
+        return getSpecialties;
+    }
+
+    public void setGetSpecialties(Set<Speciality> getSpecialties) {
+        this.getSpecialties = getSpecialties;
+    }
 }
