96d4132010fc2add8e85b70246a010cf8b9fdbe0
Marco Happe <marco.happe@icloud.com>
2020-03-04 23:23:01 +0100

    added Owners with contact info and pets on startup. closes #21

--- src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
+++ src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,8 @@
 package happe.marco.petclinic.bootstrap;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import happe.marco.petclinic.model.Owner;
+import happe.marco.petclinic.model.Pet;
 import happe.marco.petclinic.model.PetType;
 import happe.marco.petclinic.model.Vet;
 import happe.marco.petclinic.services.OwnerService;
@@ -8,6 +10,8 @@ import happe.marco.petclinic.services.PetTypeService;
 import happe.marco.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -25,23 +29,43 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        PetType marcosDog = new PetType();
-        marcosDog.setName("Senta");
-        PetType savedMarcosDog = petTypeService.save(marcosDog);
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
 
-        PetType lisasCat = new PetType();
-        lisasCat.setName("Snowball");
-        PetType savedLisasCat = petTypeService.save(lisasCat);
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
-        owner1.setFirstName("Marco");
-        owner1.setLastName("Happe");
+        owner1.setFirstName("Bart");
+        owner1.setLastName("Simpson");
+        owner1.setAddress("742 Evergreen Terrace");
+        owner1.setCity("Springfield");
+        owner1.setTelephone("9395550113");
+
+        Pet bartsPet = new Pet();
+        bartsPet.setPetType(savedDogPetType);
+        bartsPet.setOwner(owner1);
+        bartsPet.setBirthdate(LocalDate.now());
+        bartsPet.setName("Santas Little Helper");
+        owner1.getPets().add(bartsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Lisa");
         owner2.setLastName("Simpson");
+        owner2.setAddress("742 Evergreen Terrace");
+        owner2.setCity("Springfield");
+        owner2.setTelephone("9395550113");
+
+        Pet lisasPet = new Pet();
+        lisasPet.setPetType(savedCatPetType);
+        lisasPet.setOwner(owner2);
+        lisasPet.setBirthdate(LocalDate.now());
+        lisasPet.setName("Snowball II");
+        owner2.getPets().add(lisasPet);
 
         ownerService.save(owner2);
 
