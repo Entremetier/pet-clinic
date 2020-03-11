49bff4780f8c36299f430af2ce63e04eb5d022f8
Marco Happe <marco.happe@icloud.com>
2020-03-11 17:11:03 +0100

    added Spring Data JPA Service for Vets. closes #29

---
+++ src/main/java/happe/marco/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,54 @@
+package happe.marco.petclinic.services.springdatajpa;
+
+import happe.marco.petclinic.model.Vet;
+import happe.marco.petclinic.repositories.VetRepository;
+import happe.marco.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+@Service
+@Profile(value = "springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(new Consumer<Vet>() {
+            @Override
+            public void accept(Vet vet) {
+                vets.add(vet);
+            }
+        });
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
