1d2223489270ff75af3d8f742df4994e5af1d0ef
Marco Happe <marco.happe@icloud.com>
2020-03-11 18:28:38 +0100

    added Spring JPA service for visit, added visit to DataLoader.java for lisasPet. closes #39

--- src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
+++ src/main/java/happe/marco/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package happe.marco.petclinic.bootstrap;
 
 import happe.marco.petclinic.model.*;
-import happe.marco.petclinic.services.OwnerService;
-import happe.marco.petclinic.services.PetTypeService;
-import happe.marco.petclinic.services.SpecialtyService;
-import happe.marco.petclinic.services.VetService;
+import happe.marco.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -92,6 +91,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(lisasPet);
 
         ownerService.save(owner2);
+
+        Visit snowBallVisitOne = new Visit();
+        snowBallVisitOne.setPet(lisasPet);
+        snowBallVisitOne.setDate(LocalDate.now());
+        snowBallVisitOne.setDescription("Sneezy Kitten");
+
+        visitService.save(snowBallVisitOne);
 
         System.out.println("Loaded Owners....");
 
---
+++ src/main/java/happe/marco/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,49 @@
+package happe.marco.petclinic.services.springdatajpa;
+
+import happe.marco.petclinic.model.Visit;
+import happe.marco.petclinic.repositories.VisitRepository;
+import happe.marco.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile(value = "springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        //Methodenreferenz, inner anonym wird im VetSDJpaService gezeigt
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
