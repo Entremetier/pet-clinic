8d3b2fced5a2f5fd8c332da24a0e67a0d1d29724
Marco Happe <marco.happe@icloud.com>
2020-05-06 19:44:06 +0200

    implemented find owner. closes #46

--- src/main/java/happe/marco/petclinic/repositories/OwnerRepository.java
+++ src/main/java/happe/marco/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,12 @@ package happe.marco.petclinic.repositories;
 import happe.marco.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
--- src/main/java/happe/marco/petclinic/services/OwnerService.java
+++ src/main/java/happe/marco/petclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package happe.marco.petclinic.services;
 
 import happe.marco.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
